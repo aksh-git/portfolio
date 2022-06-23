aff85d987dcc7a5eb6b29ce971c6e8efcabb31fc
aksh-git <ann0m@protonmail.com>
2022-06-23 18:31:02 +0530

    mobile navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,9 @@ import { useRouter } from 'next/router';
 import { GoHome } from 'react-icons/go';
 import { BiCodeAlt, BiCertification } from 'react-icons/bi';
 import { RiUser6Line } from 'react-icons/ri';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 import { MdConnectWithoutContact } from 'react-icons/md';
+import { AiOutlineRight } from 'react-icons/ai';
 import React from 'react';
 import { useState } from 'react'
 import styles from '../styles/Navbar.module.css';
@@ -14,10 +16,12 @@ function Navbar(props) {
   let { bgColor, fgColor , acColor } = props.theme;
   const router = useRouter()
   let path = router.pathname;
-
+  const [mobnav, setmobnav] = useState("flex")
   return (<>
-    <div style={{backgroundColor:acColor+"",color:bgColor}} className={styles.navbar}>
+    <div className={styles.menuicon}><HiOutlineMenuAlt3  onClick={()=>{setmobnav(mobnav==="none"?"flex":"none")}} /></div>
+    <div style={{backgroundColor:acColor+"",color:bgColor,display:mobnav}} className={styles.navbar}>
       <nav>
+      <div onClick={()=>{setmobnav(mobnav==="none"?"flex":"none")}} className={styles.crossicon}><span>Close</span><AiOutlineRight/></div>
         <ul>
           <Link href="/" rel="noreferrer"><li className={path==="/"?"active":""}> <GoHome />Home</li></Link>
           <Link href="/project" rel="noreferrer"><li className={path==="/project"?"active":""}><BiCodeAlt />Projects</li></Link>
