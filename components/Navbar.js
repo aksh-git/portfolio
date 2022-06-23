a64ec4a48a79e7fd93e3539e864a67bce520fc97
aksh-git <ann0m@protonmail.com>
2022-06-23 22:21:09 +0530

    resume tab

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,18 +16,35 @@ function Navbar(props) {
   let { bgColor, fgColor , acColor } = props.theme;
   const router = useRouter()
   let path = router.pathname;
-  const [mobnav, setmobnav] = useState("flex")
+  const [mobnav, setmobnav] = useState("-100vw");
+  function closmobnav(){
+    setmobnav("-100vw")
+  }
   return (<>
-    <div className={styles.menuicon}><HiOutlineMenuAlt3  onClick={()=>{setmobnav(mobnav==="none"?"flex":"none")}} /></div>
-    <div style={{backgroundColor:acColor+"",color:bgColor,display:mobnav}} className={styles.navbar}>
+    <div className={styles.menuicon}><HiOutlineMenuAlt3 onClick={()=>{setmobnav(mobnav==="0"?"-100vw":"0")}} /></div>
+    <div style={{backgroundColor:acColor+"",color:bgColor}} className={styles.navbar}>
       <nav>
-      <div onClick={()=>{setmobnav(mobnav==="none"?"flex":"none")}} className={styles.crossicon}><span>Close</span><AiOutlineRight/></div>
         <ul>
           <Link href="/" rel="noreferrer"><li className={path==="/"?"active":""}> <GoHome />Home</li></Link>
           <Link href="/project" rel="noreferrer"><li className={path==="/project"?"active":""}><BiCodeAlt />Projects</li></Link>
           <Link href="/resume" rel="noreferrer"><li className={path==="/resume"?"active":""}><BiCertification />Resume</li></Link>
           <Link href="/contact" rel="noreferrer"><li className={path==="/contact"?"active":""}><MdConnectWithoutContact />Contact</li></Link>
           <Link href="/about" rel="noreferrer"><li className={path==="/about"?"active":""}><RiUser6Line />About</li></Link>
+        </ul>
+      </nav>
+      <div style={{margin:"1rem"}} className='logo'>
+        <Logo color={bgColor}/>
+      </div>
+    </div>
+    <div style={{backgroundColor:acColor+"",color:bgColor,right:mobnav}} className={`${styles.navbar} ${styles.mobnav}`}>
+      <nav>
+      <div onClick={()=>{setmobnav(mobnav==="0"?"-100vw":"0")}} className={styles.crossicon}><span>Close</span><AiOutlineRight/></div>
+        <ul>
+          <Link href="/" rel="noreferrer"><li onClick={()=>{closmobnav()}} className={path==="/"?"active":""}> <GoHome />Home</li></Link>
+          <Link href="/project" rel="noreferrer"><li onClick={()=>{closmobnav()}} className={path==="/project"?"active":""}><BiCodeAlt />Projects</li></Link>
+          <Link href="/resume" rel="noreferrer"><li onClick={()=>{closmobnav()}} className={path==="/resume"?"active":""}><BiCertification />Resume</li></Link>
+          <Link href="/contact" rel="noreferrer"><li onClick={()=>{closmobnav()}} className={path==="/contact"?"active":""}><MdConnectWithoutContact />Contact</li></Link>
+          <Link href="/about" rel="noreferrer"><li onClick={()=>{closmobnav()}} className={path==="/about"?"active":""}><RiUser6Line />About</li></Link>
         </ul>
       </nav>
       <div style={{margin:"1rem"}} className='logo'>
--- pages/resume.js
+++ pages/resume.js
@@ -1,8 +1,64 @@
 import React from 'react'
+import styles from '../styles/Resume.module.css'
+import { MdLocationPin, MdOutlineEmail } from 'react-icons/md'
+import { BsTwitter,BsFillHeartFill } from 'react-icons/bs'
+import { IoIosRocket } from 'react-icons/io'
 
-function resume() {
+function resume(props) {
+  const { bgColor, fgColor , acColor } = props.theme;
+
   return (
-    <div className='container'>resume</div>
+    <div style={{color:acColor}} className={styles.container}>
+      <h1>Akash Yadav</h1>
+      <div className={styles.resume}>
+        <div className={styles.part1}>
+          <div className={styles.ritem}>
+            <MdOutlineEmail className={styles.icon} />
+            <span>sharewithakashyadav@gmail.com</span>
+          </div>
+          <div className={styles.ritem} >
+            <MdLocationPin className={styles.icon} />
+            <span>Ludhiana, Punjab-India</span>
+          </div>
+          <div className={styles.ritem} >
+            <BsTwitter className={styles.icon} />
+            <span><a rel="noreffer" href='https://twitter.com/akash_369_'>@akash_369_</a></span>
+          </div>
+          <div className={styles.mutliItemArea}>
+            <div className={styles.rhead} >
+              <IoIosRocket className={styles.icon} />
+              <span>Skills</span>
+            </div>
+            <div className={styles.insideItem}>
+              MERN Stack | Python | Django | JavaScript | Nextjs | HTML | CSS | Git | Github | My-SQL | AWS Cloud
+            </div>
+          </div>
+          <div className={styles.mutliItemArea}>
+            <div className={styles.rhead} >
+              <BsFillHeartFill className={styles.icon} />
+              <span>Interests</span>
+            </div>
+            <div className={styles.insideItem}>
+              <div className={styles.ritem}>
+                <span>Love to code</span>
+              </div>
+              <div className={styles.ritem}>
+                <span>Astronomy</span>
+              </div>
+              <div className={styles.ritem}>
+                <span>Old Music</span>
+              </div>
+              <div className={styles.ritem}>
+                <span>Gardening : love to grow :)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.part2}>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
