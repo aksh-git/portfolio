232a67106eb7c70f944af421741c4bebf09375d2
aksh-git <ann0m@protonmail.com>
2022-07-01 22:26:47 +0530

    integrated contact api to front end

--- pages/contact.js
+++ pages/contact.js
@@ -2,11 +2,94 @@ import React, { useState } from 'react'
 import styles from '../styles/Contact.module.css'
 import Logo from '../components/Logo'
 import { ImMail4 } from 'react-icons/im'
+import Loder from '../components/Loder'
 
 function Contact(props) {
-
+  const host = "http://localhost:3000";
   const { bgColor, fgColor , acColor } = props.theme;
-  const [error, seterror] = useState("Due to some internal error, email could not be sent, please use email from app option.");
+  const [error, seterror] = useState("");
+  const [loading, setloading] = useState(0)
+  const [cname, setcname] = useState("")
+  const [cmail, setcmail] = useState("")
+  const [cmessage, setcmessage] = useState("")
+  const [csubject, setcsubject] = useState("")
+  
+  function clearinputs(){
+    setcname("");
+    setcmail("");
+    setcmessage("");
+    setcsubject("");
+  }
+
+  const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+
+  const handleInputName = (e)=>{
+    var data = e.target.value;
+    var letters = /^[a-zA-Z\s]*$/;
+    if(data.match(letters)){
+      setcname(data);
+    }
+  }
+  const handleInputMail = (e)=>{
+    var data = e.target.value;
+    setcmail(data);
+  }
+  const handleInputSubject = (e)=>{
+    var data = e.target.value;
+    setcsubject(data);
+  }
+  const handleInputMessage = (e)=>{
+    var data = e.target.value;
+    setcmessage(data);
+  }
+
+  const handleSubmitForm = async ()=>{
+    setloading(1);
+    if(!validateEmail(cmail)){
+      seterror("Please provide correct email address, so we can easily communicate easily.")
+    }else{
+      if(cname.trim().length<5){
+        seterror("Your name is too short, kindly provide your full name.")
+      }else{
+        if(csubject.trim().length<5){
+          seterror("Kindly prove a subject i.e. purpose of contact.")
+        }else{
+          if(cmessage.trim().length<10){
+            seterror("Yout message is too short.")
+          }else{
+            seterror("");
+            //API CALL
+            try {
+              const response = await fetch(`${host}/api/sendMail`,{
+                method:'POST',
+                headers: {
+                 "Accept": "*/*",
+                  'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({cmail:cmail,csubject:csubject,cname:cname,cbody:cmessage})
+              });
+              console.log(response);
+              const result = await response.json();
+              if(result.success){
+                alert("Message sent successfully.")
+                setloading(0);
+                clearinputs();
+              }else{
+                seterror(result.message);
+                setloading(0);
+              }
+            } catch (error) {
+              setloading(0);
+            }
+          }
+        }
+      }
+    }
+  }
 
   return (
     <div style={{color:acColor}} className={styles.container}>
@@ -49,20 +132,27 @@ function Contact(props) {
             <span className='inner'>Quick 
               <span> Connect.</span>
             </span>
+            {loading===1 && <div className={styles.loder}>
+              <Loder />
+            </div>}
           </div>
           <div className={styles.emailContainer}>
             {error.length>2 && 
               <div className={styles.errorTv}>
               <span>{error}</span>
               </div>}
-            <form>
-              <input type="text" placeholder='Your Name' required/>
-              <input type="email" placeholder='your_name@email.com' required/>
-              <textarea rows="4" placeholder='Please write your message here...' maxLength={500} line="4" required></textarea>
+            <form method='POST' onSubmit={(e)=>(e.preventDefault(), handleSubmitForm())}>
+              <input value={cname} onChange={handleInputName} type="text" placeholder='Your Name' required/>
+              <input value={csubject} onChange={handleInputSubject} maxLength={20} type="text" placeholder='Subject' required/>
+              <input value={cmail} onChange={handleInputMail} type="email" placeholder='your_name@email.com' required/>
+              <textarea value={cmessage} onChange={handleInputMessage} rows="4" placeholder='Please write your message here...' maxLength={500} line="4" required></textarea>
               <div className={styles.formsbtn}>
-                <button className={styles.send} disabled>Send</button>
+                <button className={styles.send}>Send</button>
               </div>
             </form>
+          </div>
+          <div className={styles.emailstatus}>
+
           </div>
           <div style={{margin:"1rem 0 0 0"}} >
             <span style={{fontSize:'12px'}}>Please provide genuine information, so that we can communicate easily.</span>
