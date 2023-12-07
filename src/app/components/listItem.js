c798070fb70652800a4dfdedcb7728a583f459c9
Akash <ann0m@protonmail.com>
2023-12-07 14:18:14 +0530

    added shallow routing

--- src/app/components/listItem.js
+++ src/app/components/listItem.js
@@ -5,16 +5,17 @@ import { useRouter } from 'next/navigation';
 
 function ListItem({ children, action, text }) {
 
-  const router = useRouter()
+  const router = useRouter();
+
   let actionFN = () => { };
   if (action?.type === "push") {
     actionFN = router.push
-  } 
+  }
   else if (action?.type === "hyperlink") {
     actionFN = router.push
   }
   return (
-    <li onClick={() => actionFN(action?.payload)} className='flex items-center gap-4 p-2 w-full cursor-pointer hover:-translate-y-1 transition-all duration-200'>
+    <li onClick={() => actionFN(action?.payload, undefined, { shallow: true })} className='flex items-center gap-4 p-2 w-full cursor-pointer hover:-translate-y-1 transition-all duration-200'>
       {children}
       <span className='text-sm hidden md:flex'>{text}</span>
     </li>
