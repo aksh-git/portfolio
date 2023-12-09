969296e35e4ede5e078cd373371a07bb50bc5242
Akash <ann0m@protonmail.com>
2023-12-09 19:02:43 +0530

    Updaded Hyperlinks

--- src/app/components/listItem.js
+++ src/app/components/listItem.js
@@ -1,25 +1,19 @@
 "use client";
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
+import React from "react";
+import Link from "next/link";
 
 function ListItem({ children, action, text }) {
-
-  const router = useRouter();
-
-  let actionFN = () => { };
-  if (action?.type === "push") {
-    actionFN = router.push
-  }
-  else if (action?.type === "hyperlink") {
-    actionFN = router.push
-  }
   return (
-    <li onClick={() => actionFN(action?.payload, undefined, { shallow: true })} className='flex items-center gap-4 p-2 w-full cursor-pointer hover:-translate-y-1 transition-all duration-200'>
-      {children}
-      <span className='text-sm hidden md:flex'>{text}</span>
-    </li>
-  )
+    <Link href={action?.payload}>
+      <li
+        className="flex items-center gap-4 p-2 w-full cursor-pointer hover:-translate-y-1 transition-all duration-200"
+      >
+        {children}
+        <span className="text-sm hidden md:flex">{text}</span>
+      </li>
+    </Link>
+  );
 }
 
 export default ListItem;
